cec5d977108466145cf2bf0d0e0fb5621822a820
IvanProtsenko <ivnprotsenko@gmail.com>
2023-07-24 13:29:56 +0300

    refactor: server removed

--- bot.js
+++ bot.js
@@ -1,10 +1,8 @@
 import dotenv from 'dotenv';
 import TelegramBot from 'node-telegram-bot-api';
 import { apiService } from './apiService.js';
-import express from 'express';
 
 dotenv.config();
-const app = express();
 
 const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
 let chatIds = [259567367, 348494653, 1629479461];
@@ -49,80 +47,63 @@ bot.on('poll', (msg) => {
 });
 
 async function poll() {
-  try {
-    const adverts = await apiService.getPollingAdverts();
-    for (let i = 0; i < adverts.length; i++) {
-      await new Promise((r) => setTimeout(r, 1000));
-      if (!adverts[i].controllersCount && !adverts[i].consoleGeneration) {
-        chatIds.forEach(async (chatId) => {
-          await bot.sendMessage(chatId, adverts[i].link);
-          await bot.sendPoll(
-            chatId,
-            `${adverts[i].adItemId}\nСколько контроллеров?`,
-            ['0', '1', '2', '3', '4'],
-            { allows_multiple_answers: false }
-          );
-          await bot.sendPoll(
-            chatId,
-            `${adverts[i].adItemId}\nТип консоли?`,
-            ['NOT_PS4', 'FAT500', 'FAT1000', 'SLIM500', 'SLIM1000', 'PRO'],
-            { allows_multiple_answers: false }
-          );
-        });
-      } else if (
-        !adverts[i].controllersCount &&
-        adverts[i].consoleGeneration != 'FAT'
-      ) {
-        chatIds.forEach(async (chatId) => {
-          await bot.sendMessage(chatId, adverts[i].link);
-          await bot.sendPoll(
-            chatId,
-            `${adverts[i].adItemId}\nСколько контроллеров?`,
-            ['0', '1', '2', '3'],
-            { allows_multiple_answers: false }
-          );
-        });
-      } else if (!adverts[i].consoleGeneration) {
-        chatIds.forEach(async (chatId) => {
-          await bot.sendMessage(chatId, adverts[i].link);
-          await bot.sendPoll(
-            chatId,
-            `${adverts[i].adItemId}\nТип консоли?`,
-            ['FAT', 'SLIM', 'PRO', 'NOT_PS4'],
-            { allows_multiple_answers: false }
-          );
+  while (true) {
+    try {
+      const adverts = await apiService.getPollingAdverts();
+      for (let i = 0; i < adverts.length; i++) {
+        await new Promise((r) => setTimeout(r, 1000));
+        if (!adverts[i].controllersCount && !adverts[i].consoleGeneration) {
+          chatIds.forEach(async (chatId) => {
+            await bot.sendMessage(chatId, adverts[i].link);
+            await bot.sendPoll(
+              chatId,
+              `${adverts[i].adItemId}\nСколько контроллеров?`,
+              ['0', '1', '2', '3', '4'],
+              { allows_multiple_answers: false }
+            );
+            await bot.sendPoll(
+              chatId,
+              `${adverts[i].adItemId}\nТип консоли?`,
+              ['NOT_PS4', 'FAT500', 'FAT1000', 'SLIM500', 'SLIM1000', 'PRO'],
+              { allows_multiple_answers: false }
+            );
+          });
+        } else if (
+          !adverts[i].controllersCount &&
+          adverts[i].consoleGeneration != 'FAT'
+        ) {
+          chatIds.forEach(async (chatId) => {
+            await bot.sendMessage(chatId, adverts[i].link);
+            await bot.sendPoll(
+              chatId,
+              `${adverts[i].adItemId}\nСколько контроллеров?`,
+              ['0', '1', '2', '3'],
+              { allows_multiple_answers: false }
+            );
+          });
+        } else if (!adverts[i].consoleGeneration) {
+          chatIds.forEach(async (chatId) => {
+            await bot.sendMessage(chatId, adverts[i].link);
+            await bot.sendPoll(
+              chatId,
+              `${adverts[i].adItemId}\nТип консоли?`,
+              ['FAT', 'SLIM', 'PRO', 'NOT_PS4'],
+              { allows_multiple_answers: false }
+            );
+          });
+        }
+        await apiService.updateAdvertByPk({
+          adItemId: adverts[i].adItemId,
+          status: 'На распознавании',
         });
       }
-      await apiService.updateAdvertByPk({
-        adItemId: adverts[i].adItemId,
-        status: 'На распознавании',
-      });
+      await apiService.reload();
+    } catch (err) {
+      console.log(err);
     }
-    await apiService.reload();
-  } catch (err) {
-    console.log(err);
+    await new Promise((r) => setTimeout(r, 60000));
   }
 }
 
-async function runInCycle() {
-  await init();
-  while (true) {
-    await poll();
-    await new Promise((r) => setTimeout(r, 5 * 60 * 1000));
-  }
-}
-
-async function run() {
-  const port = 3000;
-
-  app.get('/', async (req, res) => {
-    res.send('Hello World!');
-  });
-
-  app.listen(port, async () => {
-    console.log(`Example app listening on port ${port}`);
-    await runInCycle();
-  });
-}
-
-run();
+await init();
+await poll();
